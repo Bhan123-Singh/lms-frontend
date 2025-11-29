ff112b54513973669ea9e858d66eb0ff84d66174
Bhan123-Singh <singhbhan029@gmail.com>
2025-11-29 23:58:48 +0530

    ***

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const Base_URL="https://lms-backend-mu-five.vercel.app/api/v1";
+const Base_URL="https://lms-backend-xi-eight.vercel.app/api/v1";
 const axiosInstance=axios.create();
 axiosInstance.defaults.baseURL=Base_URL;
 axiosInstance.defaults.withCredentials=true;
